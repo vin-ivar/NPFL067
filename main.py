a7df4b025f51673c1201b6e78ec202312bdd1ba9
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-02-25 17:54:39 +0100

    stuffs

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-import sys
+import sys, codecs
 from entropy import EntropyCalc, Smoothing
 
-'''
-text = sys.stdin.read()
-for mess in ['char', 'word']:
-	for prob in [10, 5, 1, 0.1, 0.01, 0.001]:
-		e = EntropyCalc(text, mess=mess, prob=prob)
-		print("{}:{}\t{}".format(mess, prob, e.textEntropy()))
-'''
-text = sys.stdin.read()
-s = Smoothing(text)
-# s.E(debug=True)
-print(s.crossEntropy())
-# print(s.unigram['this'])
+
+if sys.argv[1] == "-a":
+
+	text = sys.stdin.read()
+	e = EntropyCalc(text, mess='none')
+	print("base:\t{}".format(e.textEntropy()))
+
+	for mess in ['char', 'word']:
+		for prob in [10, 5, 1, 0.1, 0.01, 0.001]:
+			e = EntropyCalc(text, mess=mess, prob=prob)
+			print("{}:{}\t{}".format(mess, prob, e.textEntropy()))
+
+elif sys.argv[1] == "-b":
+	text = sys.stdin.read()
+	s = Smoothing(text)
+	# s.E(debug=True)
+	print(s.crossEntropy())
+
+else:
+	print("fail")

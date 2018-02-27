fe182bd3eb76dab98ed1a21d3a485108ce312fbe
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-02-27 19:20:33 +0100

    stuffs

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import sys, codecs
+import numpy as np
 from entropy import EntropyCalc, Smoothing
-
 
 if sys.argv[1] == "-a":
 
 	text = sys.stdin.read()
 	e = EntropyCalc(text, mess='none')
 	print("base:\t{}".format(e.textEntropy()))
-
 	for mess in ['char', 'word']:
-		for prob in [10, 5, 1, 0.1, 0.01, 0.001]:
-			e = EntropyCalc(text, mess=mess, prob=prob)
-			print("{}:{}\t{}".format(mess, prob, e.textEntropy()))
+		for prob in [0.001, 0.01, 0.1, 1, 5, 10]:
+			vals = np.array([])	
+			for i in range(10):
+				e = EntropyCalc(text, mess=mess, prob=prob)
+				entropy = e.textEntropy()
+				vals = np.append(vals, entropy)
+
+			print("{}:{}\t{}\t{}\t{}".format(mess, prob, min(vals), max(vals), vals.mean()))
 
 elif sys.argv[1] == "-b":
 	text = sys.stdin.read()
